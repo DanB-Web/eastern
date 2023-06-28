7c3a95f620fb976c56eb1d9322ac4a0434c9c194
DanWeissman <dan.bayford@weissman.agency>
2023-06-28 16:20:00 +0100

    initial commit

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 export const API_URL = 'http://134.209.185.104/api/v2'
 export const SOURCE_URL = 'http://134.209.185.104'
 // NORTH, SOUTH, EAST, WEST
-export const CURRENT_TRUST = 'EAST'
+export const CURRENT_TRUST = 'WEST'
